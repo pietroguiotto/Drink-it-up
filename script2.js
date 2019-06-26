aef44831181b49ea020f21316f7a23bc76fbb037
PG-8 <40054342+PG-8@users.noreply.github.com>
2019-06-26 13:42:28 +0200

    added style2 and script2 and merged randomized histogram of monthly stats

---
+++ script2.js
@@ -0,0 +1,49 @@
+const daysOfMonth = []
+const dailyAmount = []
+
+function pushDays(){
+    for(let i = 0; i<=30; i++){
+       daysOfMonth.push(i) 
+    }
+}
+
+function populateArrayAmounts(){
+    for(let i = 0; i <= daysOfMonth.length; i++){
+        let j = Math.random() * (100 - 0) + 0;
+        let w = Math.floor(j)
+        dailyAmount.push(w)
+    }
+}
+
+function createElements(value){
+    let listItem = document.createElement("LI");
+    listItem.innerText = value;
+    document.querySelector('#chart').appendChild(listItem);
+}
+
+// function setHeight(){
+//     for(let i = 0; i < daysOfMonth.length; i++){
+//         console.log(x.textContent)
+//     }
+// }
+
+function test(){
+    const x = document.querySelectorAll('#chart li')
+    for(let i = 0; i <= daysOfMonth.length; i++){
+        let y = x[i]
+
+        y.style.paddingTop = `${(dailyAmount[i])}vh`
+        if(dailyAmount[i]<10){
+        y.textContent = `0${dailyAmount[i]}`
+    } else {
+        y.textContent = `${dailyAmount[i]}`
+
+    }
+
+    }
+}
+
+pushDays()
+populateArrayAmounts()
+daysOfMonth.forEach(createElements)
+test()
